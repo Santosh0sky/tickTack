a270a89c8df0d37b34e346eeeb1443694f393656
Santosh0sky <santosh.kr.yd.sky@gmail.com>
2025-03-01 18:51:30 +0530

    new add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import GameBoard from "./components/Gameboard";
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";  
